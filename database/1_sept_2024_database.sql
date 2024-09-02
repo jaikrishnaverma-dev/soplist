f0218b1e132272e65cfc14927469a7318a6e5218
jaikrishnaverma-dev <jaikrishnaverma@gmail.com>
2024-09-02 17:53:42 +0530

    initial commit

---
+++ database/1_sept_2024_database.sql
@@ -0,0 +1,527 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 01, 2024 at 07:56 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `billing_app`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `back_order_list`
+--
+
+CREATE TABLE `back_order_list` (
+  `id` int(30) NOT NULL,
+  `receiving_id` int(30) NOT NULL,
+  `po_id` int(30) NOT NULL,
+  `bo_code` varchar(50) NOT NULL,
+  `supplier_id` int(30) NOT NULL,
+  `amount` float NOT NULL,
+  `discount_perc` float NOT NULL DEFAULT 0,
+  `discount` float NOT NULL DEFAULT 0,
+  `tax_perc` float NOT NULL DEFAULT 0,
+  `tax` float NOT NULL DEFAULT 0,
+  `remarks` text DEFAULT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT 0 COMMENT '0 = pending, 1 = partially received, 2 =received',
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_updated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bo_items`
+--
+
+CREATE TABLE `bo_items` (
+  `bo_id` int(30) NOT NULL,
+  `item_id` int(30) NOT NULL,
+  `quantity` int(30) NOT NULL,
+  `price` float NOT NULL DEFAULT 0,
+  `unit` varchar(50) NOT NULL,
+  `total` float NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `item_list`
+--
+
+CREATE TABLE `item_list` (
+  `id` int(30) NOT NULL,
+  `name` text NOT NULL,
+  `description` text NOT NULL,
+  `supplier_id` int(30) NOT NULL,
+  `cost` float NOT NULL DEFAULT 0,
+  `hsn` varchar(100) DEFAULT NULL,
+  `gst_percent` float NOT NULL DEFAULT 0,
+  `status` tinyint(1) NOT NULL DEFAULT 1,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_updated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `item_list`
+--
+
+INSERT INTO `item_list` (`id`, `name`, `description`, `supplier_id`, `cost`, `hsn`, `gst_percent`, `status`, `date_created`, `date_updated`) VALUES
+(8, 'anurag ka shirt', 'white wala', 2, 3000, '126534163', 20, 1, '2024-09-01 23:12:01', '2024-09-01 23:12:01'),
+(9, 'ankit ka shirt', 'v++black wala', 1, 200, '2365427', 20, 1, '2024-09-01 23:14:06', '2024-09-01 23:14:06');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `po_items`
+--
+
+CREATE TABLE `po_items` (
+  `po_id` int(30) NOT NULL,
+  `item_id` int(30) NOT NULL,
+  `quantity` int(30) NOT NULL,
+  `price` float NOT NULL DEFAULT 0,
+  `unit` varchar(50) NOT NULL,
+  `total` float NOT NULL DEFAULT 0,
+  `hsn` varchar(100) DEFAULT NULL,
+  `gst_percent` int(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `po_items`
+--
+
+INSERT INTO `po_items` (`po_id`, `item_id`, `quantity`, `price`, `unit`, `total`, `hsn`, `gst_percent`) VALUES
+(25, 8, 2, 3000, 'pcs', 6000, '126534163', 20);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchase_order_list`
+--
+
+CREATE TABLE `purchase_order_list` (
+  `id` int(30) NOT NULL,
+  `po_code` varchar(50) NOT NULL,
+  `supplier_id` int(30) NOT NULL,
+  `amount` float NOT NULL,
+  `discount_perc` float NOT NULL DEFAULT 0,
+  `discount` float NOT NULL DEFAULT 0,
+  `tax_perc` float NOT NULL DEFAULT 0,
+  `is_tax_igst` tinyint(1) NOT NULL DEFAULT 0,
+  `tax` float NOT NULL DEFAULT 0,
+  `remarks` text NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT 0 COMMENT '0 = pending, 1 = partially received, 2 =received',
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_updated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `purchase_order_list`
+--
+
+INSERT INTO `purchase_order_list` (`id`, `po_code`, `supplier_id`, `amount`, `discount_perc`, `discount`, `tax_perc`, `is_tax_igst`, `tax`, `remarks`, `status`, `date_created`, `date_updated`) VALUES
+(25, 'PO-0001', 2, 7056, 2, 120, 0, 0, 1176, '', 2, '2024-09-01 23:12:32', '2024-09-01 23:12:32');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `receiving_list`
+--
+
+CREATE TABLE `receiving_list` (
+  `id` int(30) NOT NULL,
+  `form_id` int(30) NOT NULL,
+  `from_order` tinyint(1) NOT NULL DEFAULT 1 COMMENT '1=PO ,2 = BO',
+  `amount` float NOT NULL DEFAULT 0,
+  `discount_perc` float NOT NULL DEFAULT 0,
+  `discount` float NOT NULL DEFAULT 0,
+  `tax_perc` float NOT NULL DEFAULT 0,
+  `tax` float NOT NULL DEFAULT 0,
+  `stock_ids` text DEFAULT NULL,
+  `remarks` text DEFAULT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_updated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `return_list`
+--
+
+CREATE TABLE `return_list` (
+  `id` int(30) NOT NULL,
+  `return_code` varchar(50) NOT NULL,
+  `supplier_id` int(30) NOT NULL,
+  `amount` float NOT NULL DEFAULT 0,
+  `remarks` text DEFAULT NULL,
+  `stock_ids` text NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_updated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `return_list`
+--
+
+INSERT INTO `return_list` (`id`, `return_code`, `supplier_id`, `amount`, `remarks`, `stock_ids`, `date_created`, `date_updated`) VALUES
+(1, 'R-0001', 2, 3025, 'Sample Issue', '16,17', '2021-11-03 13:45:53', '2021-11-03 13:45:53'),
+(2, 'R-0002', 2, 40000, 'back order', '43', '2024-08-23 17:08:09', '2024-08-23 17:08:09');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales_list`
+--
+
+CREATE TABLE `sales_list` (
+  `id` int(30) NOT NULL,
+  `sales_code` varchar(50) NOT NULL,
+  `client` text DEFAULT NULL,
+  `amount` float NOT NULL DEFAULT 0,
+  `remarks` text DEFAULT NULL,
+  `stock_ids` text NOT NULL,
+  `is_tax_igst` tinyint(1) NOT NULL DEFAULT 0,
+  `discount` double NOT NULL DEFAULT 0,
+  `discount_perc` double NOT NULL DEFAULT 0,
+  `tax` float NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_updated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stock_list`
+--
+
+CREATE TABLE `stock_list` (
+  `id` int(30) NOT NULL,
+  `item_id` int(30) NOT NULL,
+  `quantity` int(30) NOT NULL,
+  `unit` varchar(250) DEFAULT NULL,
+  `price` float NOT NULL DEFAULT 0,
+  `total` float NOT NULL DEFAULT current_timestamp(),
+  `hsn` varchar(100) DEFAULT NULL,
+  `gst_percent` int(100) DEFAULT 0,
+  `type` tinyint(1) NOT NULL DEFAULT 1 COMMENT '1=IN , 2=OUT',
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supplier_list`
+--
+
+CREATE TABLE `supplier_list` (
+  `id` int(30) NOT NULL,
+  `name` text NOT NULL,
+  `address` text NOT NULL,
+  `cperson` text NOT NULL,
+  `contact` text NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_updated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `supplier_list`
+--
+
+INSERT INTO `supplier_list` (`id`, `name`, `address`, `cperson`, `contact`, `status`, `date_created`, `date_updated`) VALUES
+(1, 'Supplier 101', 'Sample Supplier Address 101', 'Supplier Staff 101', '09123456789', 1, '2021-11-02 09:36:19', '2021-11-02 09:36:19'),
+(2, 'Supplier 102', 'Sample Address 102', 'Supplier Staff 102', '0987654332', 1, '2021-11-02 09:36:54', '2021-11-02 09:36:54');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `system_info`
+--
+
+CREATE TABLE `system_info` (
+  `id` int(30) NOT NULL,
+  `meta_field` text NOT NULL,
+  `meta_value` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `system_info`
+--
+
+INSERT INTO `system_info` (`id`, `meta_field`, `meta_value`) VALUES
+(1, 'name', 'Stock Management System'),
+(6, 'short_name', 'SMS- PHP'),
+(11, 'logo', 'uploads/logo-1635816671.png'),
+(13, 'user_avatar', 'uploads/user_avatar.jpg'),
+(14, 'cover', 'uploads/cover-1635816671.png'),
+(15, 'content', 'Array');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(50) NOT NULL,
+  `firstname` varchar(250) NOT NULL,
+  `middlename` text DEFAULT NULL,
+  `lastname` varchar(250) NOT NULL,
+  `username` text NOT NULL,
+  `password` text NOT NULL,
+  `avatar` text DEFAULT NULL,
+  `last_login` datetime DEFAULT NULL,
+  `type` tinyint(1) NOT NULL DEFAULT 0,
+  `date_added` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `middlename`, `lastname`, `username`, `password`, `avatar`, `last_login`, `type`, `date_added`, `date_updated`) VALUES
+(1, 'Adminstrator', NULL, 'Admin', 'admin', '0192023a7bbd73250516f069df18b500', 'uploads/avatar-1.png?v=1635556826', NULL, 1, '2021-01-20 14:02:37', '2021-10-30 09:20:26'),
+(10, 'John', NULL, 'Smith', 'user1', '0192023a7bbd73250516f069df18b500', 'uploads/avatar-10.png?v=1635920488', NULL, 2, '2021-11-03 14:21:28', '2024-08-22 17:21:50'),
+(11, 'Claire', NULL, 'Blake', 'user2', '0192023a7bbd73250516f069df18b500', 'uploads/avatar-11.png?v=1635920566', NULL, 1, '2021-11-03 14:22:46', '2024-08-22 17:21:55');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_meta`
+--
+
+CREATE TABLE `user_meta` (
+  `user_id` int(30) NOT NULL,
+  `meta_field` text NOT NULL,
+  `meta_value` text NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `back_order_list`
+--
+ALTER TABLE `back_order_list`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `supplier_id` (`supplier_id`),
+  ADD KEY `po_id` (`po_id`),
+  ADD KEY `receiving_id` (`receiving_id`);
+
+--
+-- Indexes for table `bo_items`
+--
+ALTER TABLE `bo_items`
+  ADD KEY `item_id` (`item_id`),
+  ADD KEY `bo_id` (`bo_id`);
+
+--
+-- Indexes for table `item_list`
+--
+ALTER TABLE `item_list`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `supplier_id` (`supplier_id`);
+
+--
+-- Indexes for table `po_items`
+--
+ALTER TABLE `po_items`
+  ADD KEY `po_id` (`po_id`),
+  ADD KEY `item_id` (`item_id`);
+
+--
+-- Indexes for table `purchase_order_list`
+--
+ALTER TABLE `purchase_order_list`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `supplier_id` (`supplier_id`);
+
+--
+-- Indexes for table `receiving_list`
+--
+ALTER TABLE `receiving_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `return_list`
+--
+ALTER TABLE `return_list`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `supplier_id` (`supplier_id`);
+
+--
+-- Indexes for table `sales_list`
+--
+ALTER TABLE `sales_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `stock_list`
+--
+ALTER TABLE `stock_list`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `item_id` (`item_id`);
+
+--
+-- Indexes for table `supplier_list`
+--
+ALTER TABLE `supplier_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `system_info`
+--
+ALTER TABLE `system_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_meta`
+--
+ALTER TABLE `user_meta`
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `back_order_list`
+--
+ALTER TABLE `back_order_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `item_list`
+--
+ALTER TABLE `item_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `purchase_order_list`
+--
+ALTER TABLE `purchase_order_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- AUTO_INCREMENT for table `receiving_list`
+--
+ALTER TABLE `receiving_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `return_list`
+--
+ALTER TABLE `return_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `sales_list`
+--
+ALTER TABLE `sales_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `stock_list`
+--
+ALTER TABLE `stock_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
+
+--
+-- AUTO_INCREMENT for table `supplier_list`
+--
+ALTER TABLE `supplier_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `system_info`
+--
+ALTER TABLE `system_info`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `back_order_list`
+--
+ALTER TABLE `back_order_list`
+  ADD CONSTRAINT `back_order_list_ibfk_1` FOREIGN KEY (`supplier_id`) REFERENCES `supplier_list` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `back_order_list_ibfk_2` FOREIGN KEY (`po_id`) REFERENCES `purchase_order_list` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `back_order_list_ibfk_3` FOREIGN KEY (`receiving_id`) REFERENCES `receiving_list` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `bo_items`
+--
+ALTER TABLE `bo_items`
+  ADD CONSTRAINT `bo_items_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `item_list` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `bo_items_ibfk_2` FOREIGN KEY (`bo_id`) REFERENCES `back_order_list` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `item_list`
+--
+ALTER TABLE `item_list`
+  ADD CONSTRAINT `item_list_ibfk_1` FOREIGN KEY (`supplier_id`) REFERENCES `supplier_list` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `po_items`
+--
+ALTER TABLE `po_items`
+  ADD CONSTRAINT `po_items_ibfk_1` FOREIGN KEY (`po_id`) REFERENCES `purchase_order_list` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `po_items_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `item_list` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `purchase_order_list`
+--
+ALTER TABLE `purchase_order_list`
+  ADD CONSTRAINT `purchase_order_list_ibfk_1` FOREIGN KEY (`supplier_id`) REFERENCES `supplier_list` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `return_list`
+--
+ALTER TABLE `return_list`
+  ADD CONSTRAINT `return_list_ibfk_1` FOREIGN KEY (`supplier_id`) REFERENCES `supplier_list` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `stock_list`
+--
+ALTER TABLE `stock_list`
+  ADD CONSTRAINT `stock_list_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `item_list` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ initialize.php
@@ -0,0 +1,25 @@
+<?php
+$dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
+if(!defined('base_url')) define('base_url','http://localhost/soplist/');
+if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
+if(!defined('dev_data')) define('dev_data',$dev_data);  
+if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
+if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
+if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"");
+if(!defined('DB_NAME')) define('DB_NAME',"billing_app");
+?>
+
+
+
+
+
+ <?php
+// $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
+// if(!defined('base_url')) define('base_url','http://rootapi.rf.gd/');
+// if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
+// if(!defined('dev_data')) define('dev_data',$dev_data);
+// if(!defined('DB_SERVER')) define('DB_SERVER',"sql310.infinityfree.com");
+// if(!defined('DB_USERNAME')) define('DB_USERNAME',"if0_35700943");
+// if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"E4OHBadSw8q49");
+// if(!defined('DB_NAME')) define('DB_NAME',"if0_35700943_sms_db");
+?>
